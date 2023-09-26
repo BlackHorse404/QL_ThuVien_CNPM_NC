37fcd5f98e78d81987d8a45ee29b4854bb131220
Thành Tân <94952967+thanhtan34@users.noreply.github.com>
2023-09-26 15:57:40 +0700

    Create_FormEntities
    cần update trong tuần này

---
+++ QLThuVien_CNPMNC/Controls.Custom.UI/Forms.Layout.Main/FrmEntities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controls.Custom.UI.Forms.Layout.Main
+{
+    public partial class FrmEntities : Form
+    {
+        public Point mouseLocation;
+
+        public FrmEntities()
+        {
+            this.BackColor = Color.White;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.MouseDown += FrmEntities_MouseDown; 
+            this.MouseMove += FrmEntities_MouseMove; 
+        }
+
+        private void FrmEntities_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void FrmEntities_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePose = Control.MousePosition;
+                mousePose.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mousePose;
+            }
+        }
+
+       
+    }
+}
---
+++ QLThuVien_CNPMNC/QLThuVien_CNPM_NC/Form1.cs
@@ -0,0 +1,22 @@
+﻿using Controls.Custom.UI.Forms.Layout.Main;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace QLThuVien_CNPM_NC
+{
+    public partial class Form1 : FrmEntities
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
